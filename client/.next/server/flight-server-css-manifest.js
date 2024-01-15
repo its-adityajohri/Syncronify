e98588ffee1da82aa42fd84a8093189130e57640
aditya johri <aj.adityajohri@gmail.com>
2024-01-16 02:59:47 +0530

    Client updates

---
+++ client/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Aditya\\WebD_lvlup\\CS253-Project\\CS253-Main-Repo\\client\\src\\app\\layout.tsx": [
+      "C:\\Aditya\\WebD_lvlup\\CS253-Project\\CS253-Main-Repo\\client\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Aditya\\WebD_lvlup\\CS253-Project\\CS253-Main-Repo\\client\\src\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Aditya\\WebD_lvlup\\CS253-Project\\CS253-Main-Repo\\client\\src\\app\\page": [
+      "C:\\Aditya\\WebD_lvlup\\CS253-Project\\CS253-Main-Repo\\client\\src\\app\\globals.css",
+      "C:\\Aditya\\WebD_lvlup\\CS253-Project\\CS253-Main-Repo\\client\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
