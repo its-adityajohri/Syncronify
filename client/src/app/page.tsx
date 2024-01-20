d8a5d71c23228a9e4bf201109f8f4670235346f0
Deepak <deepaks.rawat144@gmail.com>
2024-01-20 19:09:50 +0530

    temporary links updated

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,18 @@
+import Link from 'next/link';
 import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1 className='text-xl m-10 p-5'>Welcome to The Landing Page</h1>
+    <div className='flex flex-col gap-5'>
+      <h1 className=''>Nav Links-</h1>
+      <Link href='dashboard'>Dashboard</Link>
+      <Link href='dashboard/navigation'>Navigation</Link>
+      <Link href='dashboard/event-page'>Events</Link>
+      <Link href='dashboard/public-channels'>Public Channel</Link>
+      <Link href='dashboard/public-events'>Public Events</Link>
+      <Link href='dashboard/user-profile'>Your Profile</Link>
+      <Link href='dashboard/your-groups'>Your Groups</Link>
+      <Link href='dashboard/your-notes'>Your Notes</Link>
     </div>
   )
 }
