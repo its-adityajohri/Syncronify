686720eae822260ca88e3c81fffe6183cb1183ec
payal540 <dangipayal24@gmail.com>
2024-01-28 21:33:07 +0530

    New Carousel

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,55 +1,68 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+'use client'
+import './Carousel.css'
+import React, { useState } from 'react';
 
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/free-mode";
+import CarouselItem from './CarouselItem';
+import CarouselIndicator from './CarouselIndicator';
 
-import { FreeMode, Pagination } from "swiper/modules";
+import { IoIosArrowBack } from 'react-icons/io';
 
-import { RxArrowTopRight } from "react-icons/rx";
-import { ServiceData } from "../constants";
+export interface CarouselProps {
+  width?: number;
+  height?: number;
+  items: React.ReactNode[];
+}
 
-const ActiveSlider = () => {
+export default function Carousel({ width, height, items }: CarouselProps) {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
+  function handleNextItemBtn() {
+    setActiveIndex((prev) => {
+      return prev + 1 < items.length ? prev + 1 : prev;
+    });
+  }
+
+  function handlePrevItemBtn() {
+    setActiveIndex((prev) => {
+      return prev - 1 >= 0 ? prev - 1 : prev;
+    });
+  }
+
   return (
-    <div className="flex items-center justify-center flex-col h-[900px] bg-[#6c34af]">
-      <Swiper
-        breakpoints={{
-          340: {
-            slidesPerView: 2,
-            spaceBetween: 15,
-          },
-          700: {
-            slidesPerView: 3,
-            spaceBetween: 15,
-          },
+    <div className="carousel-container">
+      {activeIndex > 0 && (
+        <button
+          className="carousel-btn-switch-card-left carousel-btn-switch-card"
+          onClick={handlePrevItemBtn}
+        >
+          <IoIosArrowBack />
+        </button>
+      )}
+      {items?.map((item, index) => (
+        <CarouselItem key={index} index={index} activeIndex={activeIndex}>
+          {item}
+        </CarouselItem>
+      ))}
+      {activeIndex < items.length - 1 && (
+        <button
+          className="carousel-btn-switch-card-right carousel-btn-switch-card"
+          onClick={handleNextItemBtn}
+        >
+          <IoIosArrowBack
+            style={{
+              transform: 'rotate(180deg)',
+            }}
+          />
+        </button>
+      )}
+
+      <CarouselIndicator
+        activeIndex={activeIndex}
+        length={items.length}
+        onSetActiveIndex={(activeIndex) => {
+          setActiveIndex(activeIndex);
         }}
-        freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination]}
-        className="max-w-[90%] lg:max-w-[80%]"
-      >
-        {ServiceData.map((item) => (
-          <SwiperSlide key={item.title}>
-            <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer">
-              <div
-                className="absolute inset-0 bg-cover bg-center"
-                style={{ backgroundImage: `url(${item.backgroundImage})` }}
-              />
-              <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-50" />
-              <div className="relative flex flex-col gap-3">
-                <item.icon className="text-blue-600 group-hover:text-blue-400 w-[32px] h-[32px]" />
-                <h1 className="text-xl lg:text-2xl">{item.title} </h1>
-                <p className="lg:text-[18px]">{item.content} </p>
-              </div>
-              <RxArrowTopRight className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-blue-500 group-hover:rotate-45 duration-100" />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      />
     </div>
   );
-};
-
-export default ActiveSlider;
+}
